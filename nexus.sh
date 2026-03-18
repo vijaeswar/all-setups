db7731d3c83bbdab9b4c1c03e2ed288b97f2a673
vijaeswar <88018312+vijaeswar@users.noreply.github.com>
2026-03-18 11:02:48 +0530

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -1,15 +1,15 @@
 sudo yum update -y
 sudo yum install wget -y
 sudo yum install java-17-amazon-corretto-jmods -y
-sudo mkdir /app 
-cd /app
+sudo mkdir /opt 
+cd /opt
 sudo wget https://download.sonatype.com/nexus/3/nexus-3.90.1-01-linux-x86_64.tar.gz
 sudo tar -xvf nexus-3.90.1-01-linux-x86_64.tar.gz
 sudo mv nexus-3.90.1-01 nexus
 sudo adduser nexus
-sudo chown -R nexus:nexus /app/nexus
-sudo chown -R nexus:nexus /app/sonatype*
-sudo sed -i '27  run_as_user="nexus"' /app/nexus/bin/nexus
+sudo chown -R nexus:nexus /opt/nexus
+sudo chown -R nexus:nexus /opt/sonatype*
+sudo sed -i '27  run_as_user="nexus"' /opt/nexus/bin/nexus
 sudo tee /etc/systemd/system/nexus.service > /dev/null << EOL
 [Unit]
 Description=nexus service
@@ -20,8 +20,8 @@ Type=forking
 LimitNOFILE=65536
 User=nexus
 Group=nexus
-ExecStart=/app/nexus/bin/nexus start
-ExecStop=/app/nexus/bin/nexus stop
+ExecStart=/opt/nexus/bin/nexus start
+ExecStop=/opt/nexus/bin/nexus stop
 User=nexus
 Restart=on-abort
 
